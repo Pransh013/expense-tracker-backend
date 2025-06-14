3cb691a61bde04e922d0ecef2426af4bca30462f
Pranshu Verma <pranshuverma1601@gmail.com>
2025-06-14 17:47:33 +0530

    feat: add prisma setup

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
+import { prisma } from "@/config/prisma";
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -8,8 +9,10 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get("/", (_: Request, res: Response) => {
-  res.json({ message: "Hello World!!!" });
+app.post("/api/v1/users", async (req: Request, res: Response) => {
+  const { name, email } = req.body;
+  const user = await prisma.user.create({ data: { name, email } });
+  res.status(200).json({ message: "user created", user });
 });
 
 app.listen(PORT, () => {
